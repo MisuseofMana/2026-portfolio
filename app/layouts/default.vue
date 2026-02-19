4145ccd22b2e76c67fabad8fea4bd4335fdf7c0c
Sean Yager <seanyagerart@gmail.com>
2026-02-18 22:39:54 -0600

    bolster projects, install icons, add background image

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,10 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
-    <UContainer class="sm:border-x border-default pt-10">
+    <UContainer
+      class="sm:border-x border-neutral-800 pt-10 min-h-screen bg-white/90 dark:bg-[#091020]/80"
+    >
       <AppHeader :links="navLinks" />
       <slot />
       <AppFooter />
--- app/pages/projects.vue
+++ app/pages/projects.vue
@@ -14,140 +14,6 @@ const { data: projects } = await useAsyncData("projects", () => {
   return queryCollection("projects").all();
 });
 
-const testimonials = ref([
-  {
-    user: {
-      name: "Evan You",
-      description: "Author of Vue.js and Vite",
-      avatar: {
-        src: "https://avatars.githubusercontent.com/u/499550?v=4",
-        alt: "Evan You",
-      },
-    },
-    quote: "Nuxt on Cloudflare infra with minimal effort - this is huge!",
-  },
-  {
-    user: {
-      name: "Igor Minar",
-      description: "Software Engineer at Cloudflare",
-      avatar: {
-        src: "https://avatars.githubusercontent.com/u/216296?v=4",
-        alt: "Igor Minar",
-      },
-    },
-    quote:
-      "I love the polish and the batteries-included approach. NuxtHub takes web framework and hosting integration to a new level!",
-  },
-  {
-    user: {
-      name: "Charlie Hield",
-      description: "Senior Creative Technologist",
-      avatar: {
-        src: "https://avatars.githubusercontent.com/u/527849?v=4",
-        alt: "Charlie Hield",
-      },
-    },
-    quote:
-      "NuxtHub is hands down the easiest way to get a project from zero to production on the Cloudflare stack!",
-  },
-  {
-    user: {
-      name: "Israel Ortuño",
-      description: "Co-founder of VueJobs",
-      avatar: {
-        src: "https://avatars.githubusercontent.com/u/1769417?v=4",
-        alt: "Israel Ortuño",
-      },
-    },
-    quote:
-      "I can't find an excuse to not go full-stack with Nuxt from now on. Ship fast the Nuxt way, zero config. Just plug & deploy.",
-  },
-  {
-    user: {
-      name: "Fayaz Ahmed",
-      description: "Indie Hacker",
-      avatar: {
-        src: "https://avatars.githubusercontent.com/u/15716057?v=4",
-        alt: "Fayaz Ahmed",
-      },
-    },
-    quote:
-      "Took me less than 90 seconds to deploy an app with DB, KV, File storage and Caching, all on the edge with just a single command.",
-  },
-  {
-    user: {
-      name: "Tommy J. Vedvik",
-      description: "UX Developer",
-      avatar: {
-        src: "https://avatars.githubusercontent.com/u/48070?v=4",
-        alt: "Tommy J. Vedvik",
-      },
-    },
-    quote:
-      "Nuxt is becoming the best framework for bootstrappers imo. NuxtHub is a layer on top of Cloudflare services for cheap & fast full-stack edge hosting.",
-  },
-  {
-    user: {
-      name: "Dario Piotrowicz",
-      description: "Web Developer at Cloudflare",
-      avatar: {
-        src: "https://avatars.githubusercontent.com/u/61631103?v=4",
-        alt: "Dario Piotrowicz",
-      },
-    },
-    quote:
-      "I love how NuxtHub combines, amplifies and simplifies the Cloudflare tooling with the wide and mature Nuxt ecosystem. I cannot wait to see how it will evolve and expand in the future!",
-  },
-  {
-    user: {
-      name: "Markus Oberlehner",
-      description: "Web Developer",
-      avatar: {
-        src: "https://avatars.githubusercontent.com/u/6883314?v=4",
-        alt: "Markus Oberlehner",
-      },
-    },
-    quote:
-      "Just deployed my first site to Cloudflare using NuxtHub. Very sleek experience!",
-  },
-  {
-    user: {
-      name: "Anthony Fu",
-      description: "Core team Vue.js, Vite & Nuxt",
-      avatar: {
-        src: "https://avatars.githubusercontent.com/u/11247099?v=4",
-        alt: "Anthony Fu",
-      },
-    },
-    quote:
-      "It's amazing to be able to run a single command and get existing Nuxt project deployed on edge within minutes! It felt like unlocking the missing infrastructure and UI for Cloudflare, enhancing the developer experience in such an extraordinary way.",
-  },
-  {
-    user: {
-      name: "Jonathan Beckman",
-      description: "Founder of GuaranTee Time",
-      avatar: {
-        src: "https://avatars.githubusercontent.com/u/90707158?v=4",
-        alt: "Jonathan Beckman",
-      },
-    },
-    quote:
-      "NuxtHub and Cloudflare are my go to for full stack apps. The DX is joyous and far superior to any other platform I've used. My team is able to iterate quickly, and build beautiful, performant apps with ease.",
-  },
-  {
-    user: {
-      name: "Eckhardt Dreyer",
-      description: "Lead Developer at YG",
-      avatar: {
-        src: "https://avatars.githubusercontent.com/u/37825447?v=4",
-        alt: "Eckhardt Dreyer",
-      },
-    },
-    quote:
-      "At YG, our team recently grew and that meant more seats on all the tools we use. Migrating our hosting workflow to NuxtHub not only took just a few minutes but saved us money from our previous provider. NuxtHub provides an excellent management layer on top of our infrastructure and we're super happy about the move!",
-  },
-]);
-
 useSeoMeta({
   title: page.value?.seo?.title || page.value?.title,
   ogTitle: page.value?.seo?.title || page.value?.title,
@@ -178,26 +44,42 @@ useSeoMeta({
           v-for="project in projects"
           :key="project.title"
           variant="subtle"
-          to="https://crickburrow.com"
+          :to="project.url"
           target="_blank"
           :title="project.title"
           :ui="{ leadingIcon: 'size-10' }"
         >
           <template #leading>
             <img
-              src="/projects/sigil.png"
+              :src="project.image"
               alt="Crickburrow Archives"
               class="w-full"
             />
           </template>
           <template #description>{{ project.description }}</template>
           <template #footer>
-            <UBadge color="neutral" variant="subtle" class="text-sm">
-              Updated:
+            <UBadge
+              v-for="tag in project.tags"
+              :key="tag"
+              :leading-icon="tag.icon"
+              color="neutral"
+              variant="outline"
+              class="text-md mr-1 mb-1"
+              :ui="{ leadingIcon: 'size-5 mr-1' }"
+            >
+              {{ tag.text }}
+            </UBadge>
+            <UBadge
+              color="info"
+              leading-icon="i-custom-time"
+              variant="outline"
+              class="text-md mb-1"
+              :ui="{ leadingIcon: 'size-5 mr-1' }"
+            >
               {{
-                new Date(project.date).toLocaleString("default", {
+                new Date(project.date).toLocaleDateString("default", {
                   month: "short",
-                  year: "2-digit",
+                  year: "numeric",
                 })
               }}
             </UBadge>
